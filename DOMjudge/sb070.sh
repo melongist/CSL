851f4291cd2de57448b0cd206a89b9a02cea4206
melongist <what_is_computer@msn.com>
2022-01-18 19:56:51 +0900

    21.01.18 update
    21.01.18 update

--- DOMjudge/sb070.sh
+++ DOMjudge/sb070.sh
@@ -3,8 +3,13 @@
 #https://github.com/spotboard
 #https://github.com/spotboard/spotboard
 
+#DOMjudge judgehost starting script
+#DOMjudge7.3.4 stable + Ubuntu 20.04 LTS
+#Made by 
+#2022.01.18 melongist(melongist@gmail.com, what_is_computer@msn.com) for CS teachers
 
-#spotboard 0.7.0 for domjudge7.3.3 + Ubuntu 20.04 LTS Server
+
+#spotboard 0.7.0 for domjudge7.3.4 + Ubuntu 20.04 LTS Server
 
 #terminal commands to install spotboard webapp
 #wget https://raw.githubusercontent.com/melongist/CSL/master/DOMjudge/sb070.sh
@@ -30,22 +35,42 @@ mv spotboard-webapp-0.7.0 spotboard
 sudo mv spotboard /var/www/html/
 cd /var/www/html/spotboard/
 
-sudo apt -y install nodejs
-sudo apt -y install npm
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+#sudo apt -y install nodejs
+#sudo apt -y install npm
+
+
 sudo npm install -g npm
 sudo npm install -g grunt-cli
 npm install
 npm run build
 
+
 sed -i "s#feed_server_path = './sample/'#feed_server_path = './'#" /var/www/html/spotboard/dist/config.js
 sed -i "s#'award_slide.json' :'./sample/award_slide.json'#'award_slide.json' :'./award_slide.json'#" /var/www/html/spotboard/dist/config.js
 sed -i "s#animation          : false#animation          : true#" /var/www/html/spotboard/dist/config.js
+
+
+#nodejs stable update
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
+node -v
+
+#npm update
+sudo npm i -g npm
+npm -v
+
+
 
 clear
 
 echo "" | tee -a domjudge.txt
 echo "spotboard for domjudge installed!!" | tee -a domjudge.txt
-echo "Ver 2020.10.19" | tee -a domjudge.txt
+echo "Ver 2022.01.18" | tee -a domjudge.txt
 echo "" | tee -a domjudge.txt
 echo "Check spotboard!" | tee -a domjudge.txt
 echo "------" | tee -a domjudge.txt
