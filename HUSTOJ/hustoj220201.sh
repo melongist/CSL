c801fc9ef75174727f72200f1e7909011665c006
melongist <what_is_computer@msn.com>
2022-02-11 18:56:56 +0900

    22.02.11 update
    22.02.11 update

--- HUSTOJ/hustoj220201.sh
+++ HUSTOJ/hustoj220201.sh
@@ -34,12 +34,6 @@ apt update
 apt -y upgrade
 apt -y autoremove
 
-apt -y install apt-transport-https ca-certificates curl gnupg-agent lsb-release software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt-cache policy docker-ce
-apt -y install docker-ce docker-ce-cli containerd.io
-
 apt -y install subversion zip unzip
 
 /usr/sbin/useradd -m -u 1536 judge
