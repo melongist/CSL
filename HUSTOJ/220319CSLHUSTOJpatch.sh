24fc86f2882abd45c1c5f8a6b39a3574e0a941f8
melongist <what_is_computer@msn.com>
2022-03-19 06:16:01 +0900

    22.03.19 update
    - xml image download error patch

---
+++ HUSTOJ/220319CSLHUSTOJpatch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#CSL HUSTOJ
+#Made by melongist(what_is_computer@msn.com)
+#for CSL Computer Science teachers
+
+clear
+
+THISFILE="220319CSLHUSTOJpatch.sh"
+
+if [[ -z $SUDO_USER ]]
+then
+  echo "Use 'sudo bash ${THISFILE}'"
+  exit 1
+fi
+
+echo ""
+echo "---- CSL HUSTOJ xml image download error patch ----"
+echo ""
+
+#problem_export.php customizing for front, rear, bann, credits fields
+wget https://raw.githubusercontent.com/melongist/CSL/master/HUSTOJ/admin/problem_export220223.php
+mv -f ./problem_export220223.php /home/judge/src/web/admin/problem_export.php
+chown www-data:root /home/judge/src/web/admin/problem_export.php
+chmod 664 /home/judge/src/web/admin/problem_export.php
+
+echo ""
+echo "---- CSL HUSTOJ xml image download error patched ----"
+echo ""
