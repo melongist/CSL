73402c0587b203cae1743a65f68d5eb77b9358e6
melongist <what_is_computer@msn.com>
2022-05-18 15:44:50 +0900

    22.05.18 update
    22.05.18 update

--- CMS/cms15dev02.sh
+++ CMS/cms15dev02.sh
@@ -48,6 +48,7 @@ sudo sed -i "s#commands.append(\[\"\/usr\/bin\/python3\"#commands.append(\[\"\/u
 sudo sed -i "s#return \[\[\"\/usr\/bin\/python3\"#return \[\[\"\/usr\/bin\/pypy3\"#g" ~/cms/cms/grading/languages/python3_pypy3.py
 
 
+cd
 cd cms
 sudo python3 setup.py install
 cd
@@ -57,35 +58,43 @@ cmsInitDB
 cmsAddAdmin admin -p $USERPW
 
 echo "cms1.5.dev0 installation completed!!" | tee -a cms.txt
-echo "Ver 2022.05.18 CSL" | tee -a cms.txt
+echo "Last update : 2022.05.18" | tee -a cms.txt
 echo "" | tee -a cms.txt
-echo "------ After every reboot ------" | tee -a cms.txt
-echo "For CMS admin page" | tee -a cms.txt
-echo "run   : cmsAdminWebServer" | tee -a cms.txt
+echo "------ CMS admin server ------" | tee -a cms.txt
+echo "Start CMS admin server" | tee -a cms.txt
+echo "  PC terminal?  : cmsAdminWebServer" | tee -a cms.txt
+echo "  AWS terminal? : setsid cmsAdminWebServer &" | tee -a cms.txt
+echo "" | tee -a cms.txt
 echo "      id : admin" | tee -a cms.txt
 echo "      pw : $USERPW" | tee -a cms.txt
 echo "" | tee -a cms.txt
-echo "make  : contest, user ... with contest admin menu" | tee -a cms.txt
-echo "      http://localhost:8889" | tee -a cms.txt
 echo "" | tee -a cms.txt
-echo "For service monitoring" | tee -a cms.txt
-echo "run   : cmsResourceService -a" | tee -a cms.txt
-echo "and   select # to start contest!" | tee -a cms.txt
+echo "------ Making contest ------" | tee -a cms.txt
+echo "At admin page" | tee -a cms.txt
+echo "http://localhost:8889" | tee -a cms.txt
+echo "  upload tasks ... attachments ... in/out files ..." | tee -a cms.txt
+echo "  select/make scoring method ... etc" | tee -a cms.txt
+echo "  make user accounts ... etc ..." | tee -a cms.txt
+echo "" | tee -a cms.txt
+echo "" | tee -a cms.txt
+echo "------ Run contest ------" | tee -a cms.txt
+echo "  PC terminal?  : cmsResourceService -a" | tee -a cms.txt
+echo "  AWS terminal? : cmsResourceService -a" | tee -a cms.txt
+echo "  and   select # to start contest!" | tee -a cms.txt
 echo "" | tee -a cms.txt
 echo "check : contest, user ... with user menu" | tee -a cms.txt
 echo "      http://localhost:8888" | tee -a cms.txt
 echo "  how to change port number : sudo nano /usr/local/etc/cms.conf" | tee -a cms.txt
 echo "" | tee -a cms.txt
+echo "------ CMS Ranking Web Server ------" | tee -a cms.txt
+echo "Start CMS Ranking Web Server" | tee -a cms.txt
+echo "  PC terminal?  : cmsRankingWebServer" | tee -a cms.txt
+echo "  AWS terminal? : setsid cmsRankingWebServer &" | tee -a cms.txt
+echo "" | tee -a cms.txt
+echo "" | tee -a cms.txt
 
-cmsAdminWebServer
 
-
-#for admin page service
-#setsid cmsAdminWebServer &
-
-#for ranking page service
-#setsid cmsRankingWebServer &
-
-#for run contest
-#cmsResourceService -a
-#and select contest number
+clear
+echo "------ CMS 1.5.dev0 ready!! ------"
+echo ""
+cat cms.txt
