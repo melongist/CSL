bdd70fe71ec8d18a823ec95e4e5cf5eb80a7869c
melongist <what_is_computer@msn.com>
2022-06-06 09:43:07 +0900

    22.06.06 update
    22.06.06 update

---
+++ DOMjudge/dj810dj.sh
@@ -0,0 +1,217 @@
+#!/bin/bash
+
+#origin
+#https://www.domjudge.org/
+#https://github.com/DOMjudge/domjudge
+
+#DOMjudge server installation script
+#DOMjudge8.1.0 stable + Ubuntu 22.04 LTS
+#2022.06.06 Made by melongist(melongist@gmail.com, what_is_computer@msn.com) for CS teachers
+
+#terminal commands to install DOMjudge server
+#------
+#wget https://raw.githubusercontent.com/melongist/CSL/master/DOMjudge/dj810dj.sh
+#bash dj810dj.sh
+
+
+if [[ $SUDO_USER ]] ; then
+  echo "Just use 'bash dj810dj.sh'"
+  exit 1
+fi
+
+cd
+
+#change your timezone
+#for South Korea's timezone
+sudo timedatectl set-timezone 'Asia/Seoul'
+
+sudo apt update
+sudo apt -y upgrade
+
+sudo apt -y install software-properties-common dirmngr apt-transport-https
+sudo apt -y install acl
+sudo apt -y install zip
+
+sudo apt -y install mariadb-server mariadb-client
+
+#You must input mariaDB's root account password! #1
+sudo mysql_secure_installation
+
+
+#apache2
+sudo apt install -y apache2
+sudo apt install -y php8.1
+
+#nginx
+#sudo apt install -y nginx
+#sudo systemctl enable nginx
+
+#sudo apt install php
+#sudo apt install php-fpm
+sudo apt install -y php8.1-fpm
+#sudo apt install php-gd
+sudo apt install -y php8.1-gd
+#sudo apt install php-cli
+#sudo apt install php-intl
+sudo apt install -y php8.1-intl
+#sudo apt install php-mbstring
+sudo apt install -y php8.1-mbstring
+#sudo apt install php-mysql
+sudo apt install -y php8.1-mysql
+#sudo apt install php-curl
+sudo apt install -y php8.1-curl
+#sudo apt install php-json
+#sudo apt install -y php8.1-json
+#sudo apt install php-xml
+sudo apt install -y php8.1-xml
+#sudo apt install php-zip
+sudo apt install -y php8.1-zip
+
+
+sudo apt -y install composer
+sudo apt -y install ntp
+sudo apt -y install build-essential
+sudo apt -y install libcgroup-dev
+#sudo apt -y install libcurl4-openssl-dev
+sudo apt -y install libcurl4-gnutls-dev
+sudo apt -y install libjsoncpp-dev
+
+#nginx
+#sudo apt -y install apache2-utils
+
+
+#DOMjudge 8.1.0 stable
+#wget https://www.domjudge.org/releases/domjudge-8.1.0.tar.gz
+#tar xvf domjudge-8.1.0.tar.gz
+#cd domjudge-8.1.0
+
+#DOMjudge 8.1.0 stable
+wget https://raw.githubusercontent.com/melongist/CSL/master/DOMjudge/domjudge-8.1.0.tar.gz
+tar xvf domjudge-8.1.0.tar.gz
+rm domjudge-8.1.0.tar.gz
+cd domjudge-8.1.0
+
+./configure --with-baseurl=BASEURL
+make domserver
+sudo make install-domserver
+
+cd /opt/domjudge/domserver/bin
+#./dj_setup_database genpass # it's not required..
+
+#Use mariaDB's root password above #1
+sudo ./dj_setup_database -u root -r install
+
+
+
+
+#apache2
+sudo ln -s -f /opt/domjudge/domserver/etc/apache.conf /etc/apache2/conf-available/domjudge.conf
+sudo ln -s -f /opt/domjudge/domserver/etc/domjudge-fpm.conf /etc/php/8.1/fpm/pool.d/domjudge.conf
+sudo a2enmod proxy_fcgi setenvif rewrite
+sudo systemctl restart apache2
+sudo a2enconf php8.1-fpm domjudge
+sudo systemctl reload apache2
+sudo service php8.1-fpm reload
+sudo service apache2 reload
+
+
+#nginx
+#nginx
+#sudo ln -s /opt/domjudge/domserver/etc/nginx-conf /etc/nginx/sites-enabled/domjudge
+#sudo ln -s /opt/domjudge/domserver/etc/domjudge-fpm.conf /etc/php/7.4/fpm/pool.d/domjudge.conf
+#sudo service php7.4-fpm reload
+#sudo service nginx reload
+#sudo sed -i "s:index index.html:index index.html index.php:g" /etc/nginx/sites-enabled/default
+#sudo sed -i "s:#location ~ \\\.php\\$:location ~ \\\.php\\$:g" /etc/nginx/sites-enabled/default
+#sudo sed -i "s:#\tinclude snippets:\tinclude snippets:g" /etc/nginx/sites-enabled/default
+#sudo sed -i "s|#\tfastcgi_pass unix|\tfastcgi_pass unix|g" /etc/nginx/sites-enabled/default
+#sudo sed -i "s|9000;|9000;\n\t}|g" /etc/nginx/sites-enabled/default
+#sudo service php7.4-fpm reload
+#sudo service nginx reload
+
+
+#For DOMjudge configuration check
+#apache2
+#MariaDB Max connections to 10000
+sudo sed -i "s/\#max_connections        = 100/max_connections        = 10000/" /etc/mysql/mariadb.conf.d/50-server.cnf
+#PHP upload_max_filesize to 512M
+sudo sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 512M/" /etc/php/8.1/apache2/php.ini
+#PHP max_file_uploads to 512
+sudo sed -i "s/max_file_uploads = 20/max_file_uploads = 512/" /etc/php/8.1/apache2/php.ini
+#PHP post_max_size to 512M
+sudo sed -i "s/post_max_size = 8M/post_max_size = 512M/" /etc/php/8.1/apache2/php.ini
+#PHP memory_limit to 2048M
+sudo sed -i "s/memory_limit = 128M/memory_limit = 2048M/" /etc/php/8.1/apache2/php.ini
+
+
+
+#option
+#making auto direction
+cd
+sudo rm -f /var/www/html/index.html
+echo "<script>document.location=\"./domjudge/\";</script>" > index.html
+sudo chmod 644 index.html
+sudo chown root:root index.html
+sudo mv index.html /var/www/html/
+
+sudo apt -y autoremove
+
+
+PASSWORD=$(cat /opt/domjudge/domserver/etc/initial_admin_password.secret)
+
+echo "" | tee -a domjudge.txt
+echo "DOMjudge 8.1.0 stable 22.06.06" | tee -a domjudge.txt
+echo "" | tee -a domjudge.txt
+echo "DOMserver installed!!" | tee -a domjudge.txt
+echo "" | tee -a domjudge.txt
+echo "Check below to access DOMserver's web interface!" | tee -a domjudge.txt
+echo "------" | tee -a domjudge.txt
+echo "http://localhost/domjudge/" | tee -a domjudge.txt
+echo "admin ID : admin" | tee -a domjudge.txt
+echo "admin PW : $PASSWORD" | tee -a domjudge.txt
+echo ""
+
+#scripts set download
+wget https://raw.githubusercontent.com/melongist/CSL/master/DOMjudge/dj810jh.sh
+wget https://raw.githubusercontent.com/melongist/CSL/master/DOMjudge/dj810kr.sh
+wget https://raw.githubusercontent.com/melongist/CSL/master/DOMjudge/dj810start.sh
+wget https://raw.githubusercontent.com/melongist/CSL/master/DOMjudge/dj810clear.sh
+
+bash dj810jh.sh
+
+echo "" | tee -a domjudge.txt
+echo "judgehosts installed!!" | tee -a domjudge.txt
+echo "" | tee -a domjudge.txt
+echo "" | tee -a domjudge.txt
+echo "------ Run judgehosts script after every reboot ------" | tee -a domjudge.txt
+echo "bash dj810start.sh" | tee -a domjudge.txt
+echo "" | tee -a domjudge.txt
+echo "------ Run DOMjudge cache clearing script when needed ------" | tee -a domjudge.txt
+echo "bash dj810clear.sh" | tee -a domjudge.txt
+echo "" | tee -a domjudge.txt
+echo "------ etc ------" | tee -a domjudge.txt
+echo "How to kill some judgedaemon processe?" | tee -a domjudge.txt
+echo "ps -ef, and find PID# of judgedaemon, run : sudo kill -15 PID#" | tee -a domjudge.txt
+echo "" | tee -a domjudge.txt
+echo "How to clear domserver web cache?" | tee -a domjudge.txt
+echo "sudo rm -rf /opt/domjudge/domserver/webapp/var/cache/prod/*" | tee -a domjudge.txt
+echo "" | tee -a domjudge.txt
+echo "How to clear DOMjudge cache??" | tee -a domjudge.txt
+echo "sudo /opt/domjudge/domserver/webapp/bin/console cache:clear" | tee -a domjudge.txt
+echo "" | tee -a domjudge.txt
+chmod 660 domjudge.txt
+echo "Saved as domjudge.txt"
+
+echo ''
+echo 'System will be rebooted in 20 seconds!'
+echo ''
+COUNT=20
+while [ $COUNT -ge 0 ]
+do
+  echo $COUNT
+  ((COUNT--))
+  sleep 1
+done
+echo 'rebooted!'
+sleep 5
+sudo reboot
---
+++ DOMjudge/dj810jh.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+#origin
+#https://www.domjudge.org/
+#https://github.com/DOMjudge/domjudge
+
+#DOMjudge server installation script
+#DOMjudge8.1.0 stable + Ubuntu 22.04 LTS
+#2022.06.06 Made by melongist(melongist@gmail.com, what_is_computer@msn.com) for CS teachers
+
+#terminal commands to install DOMjudge judgehosts
+#------
+#wget https://raw.githubusercontent.com/melongist/CSL/master/DOMjudge/dj810jh.sh
+#bash dj810jh.sh
+
+
+if [[ $SUDO_USER ]] ; then
+  echo "Just use 'bash dj810jh.sh'"
+  exit 1
+fi
+
+cd
+
+sudo apt update
+sudo apt -y upgrade
+
+sudo apt -y install make
+sudo apt -y install pkg-config
+sudo apt -y install sudo
+sudo apt -y install debootstrap
+sudo apt -y install libcgroup-dev
+sudo apt -y install php8.1-cli
+sudo apt -y install php8.1-curl
+#sudo apt -y install php8.1-json
+sudo apt -y install php8.1-xml
+sudo apt -y install php8.1-zip
+sudo apt -y install lsof
+sudo apt -y install procps
+
+#for ubuntu
+sudo apt -y remove apport
+
+
+#pypy3
+sudo apt -y install pypy3
+#java
+sudo apt -y install default-jre-headless
+sudo apt -y install default-jdk-headless
+#haskell
+sudo apt -y install ghc
+#pascal
+sudo apt -y install fp-compiler
+#JavaScript
+sudo apt -y install nodejs
+sudo apt -y install npm
+#R
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+#sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
+sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+sudo apt -y update
+sudo apt -y install r-base
+#swift
+sudo apt -y install clang libicu-dev
+#wget https://download.swift.org/swift-5.6.1-release/ubuntu2004/swift-5.6.1-RELEASE/swift-5.6.1-RELEASE-ubuntu22.04.tar.gz
+wget https://download.swift.org/development/ubuntu2204/swift-DEVELOPMENT-SNAPSHOT-2022-06-02-a/swift-DEVELOPMENT-SNAPSHOT-2022-06-02-a-ubuntu22.04.tar.gz
+
+tar -zxvf swift-DEVELOPMENT-SNAPSHOT-2022-06-02-a-ubuntu22.04.tar.gz
+rm swift-DEVELOPMENT-SNAPSHOT-2022-06-02-a-ubuntu22.04.tar.gz
+sudo mv ~/swift-DEVELOPMENT-SNAPSHOT-2022-06-02-a-ubuntu22.04 ~/swift
+sudo ln -s ~/swift/usr/bin/swiftc /usr/bin/swiftc
+
+
+#DOMjudge 8.1.0 stable
+cd domjudge-8.1.0
+./configure --prefix=/opt/domjudge --with-baseurl=BASEURL
+
+#make judgehost
+make judgehost
+sudo make install-judgehost
+
+#judgehosts
+#defaul judgedaemon
+sudo useradd -d /nonexistent -U -M -s /bin/false domjudge-run
+#multiple judgedaemons, bound to a core, max 128
+for ((i=0; i<128; i++));
+do
+  sudo useradd -d /nonexistent -U -M -s /bin/false domjudge-run-$i
+done
+
+sudo cp /opt/domjudge/judgehost/etc/sudoers-domjudge /etc/sudoers.d/
+sudo chmod 0440 /etc/sudoers.d/sudoers-domjudge
+
+#try #1 for Ubuntu Desktop
+sudo sed -i "s#GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash\"#GRUB_CMDLINE_LINUX_DEFAULT=\"quiet cgroup_enable=memory swapaccount=1 isolcpus=2\"#" /etc/default/grub
+#try #2 for Ubuntu Server
+sudo sed -i "s#GRUB_CMDLINE_LINUX_DEFAULT=\"\"#GRUB_CMDLINE_LINUX_DEFAULT=\"quiet cgroup_enable=memory swapaccount=1 isolcpus=2\"#" /etc/default/grub
+#try #3 AWS Ubuntu 22.04 LTS Server
+if [ -f /etc/default/grub.d/50-cloudimg-settings.cfg ]; then
+	echo "Editing /etc/default/grub.d/50-cloudimg-settings.cfg for AWS"
+  sudo sed -i "s#GRUB_CMDLINE_LINUX_DEFAULT=\"console=tty1 console=ttyS0 nvme_core.io_timeout=4294967295\"#GRUB_CMDLINE_LINUX_DEFAULT=\"quiet cgroup_enable=memory swapaccount=1 isolcpus=2\"#" /etc/default/grub.d/50-cloudimg-settings.cfg
+fi
+
+
+#make docs
+sudo apt -y install python3-sphinx python3-sphinx-rtd-theme rst2pdf fontconfig python3-yaml
+make docs
+sudo make install-docs
+
+sudo update-grub
+
+#make chroot
+#default
+#sudo /opt/domjudge/judgehost/bin/dj_make_chroot
+
+#default + JavaScript,R,swift
+echo 'y' | sudo /opt/domjudge/judgehost/bin/dj_make_chroot -i nodejs,r-base,swift,pypy3
+
+
+sudo apt -y autoremove
