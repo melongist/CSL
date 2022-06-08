76ea44f9cc1b4287578bb0b61624c3f630096563
melongist <what_is_computer@msn.com>
2022-06-08 23:12:43 +0900

    22.06.06 update
    22.06.06 update

---
+++ HUSTOJ/admin/problem_add_page220606.php
@@ -0,0 +1,126 @@
+<html>
+<head>
+  <meta http-equiv="Pragma" content="no-cache">
+  <meta http-equiv="Cache-Control" content="no-cache">
+  <meta http-equiv="Content-Language" content="zh-cn">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>Problem Add</title>
+</head>
+<hr>
+
+<?php 
+  require_once("../include/db_info.inc.php");
+  require_once("admin-header.php");
+  if (!(isset($_SESSION[$OJ_NAME.'_'.'administrator']) || isset($_SESSION[$OJ_NAME.'_'.'contest_creator']) || isset($_SESSION[$OJ_NAME.'_'.'problem_editor']))) {
+    echo "<a href='../loginpage.php'>Please Login First!</a>";
+    exit(1);
+  }
+  echo "<center><h3>".$MSG_PROBLEM."-".$MSG_ADD."</h3></center>";
+  include_once("kindeditor.php") ;
+?>
+
+<body leftmargin="30" >
+  <div class="container">
+    <form method=POST action=problem_add.php>
+      <input type=hidden name=problem_id value="New Problem">
+        <p align=left>
+          <?php echo "<h3>".$MSG_TITLE."</h3>"?>
+          <input class="input input-xxlarge" style="width:100%;" type=text name=title><br><br>
+        </p>
+        <p align=left>
+          <?php echo $MSG_Time_Limit?><br>
+          <input class="input input-mini" type=number min="0.001" max="300" step="0.001" name=time_limit size=20 value=1> sec<br><br>
+          <?php echo $MSG_Memory_Limit?><br>
+          <input class="input input-mini" type=number min="1" max="1024" step="1" name=memory_limit size=20 value=128> MiB<br><br>
+        </p>
+ 
+        <p align=left>
+          <?php echo "<h4>".$MSG_Description."</h4>"?>
+          <textarea class="kindeditor" rows=13 name=description cols=80></textarea><br>
+        </p>
+        <p align=left>
+          <?php echo "<h4>".$MSG_Input."</h4>"?>
+          <textarea class="kindeditor" rows=13 name=input cols=80></textarea><br>
+        </p>
+        <p align=left>
+          <?php echo "<h4>".$MSG_Output."</h4>"?>
+          <textarea  class="kindeditor" rows=13 name=output cols=80></textarea><br>
+        </p>
+        <p align=left>
+          <?php echo "<h4>".$MSG_Sample_Input."</h4>"?>
+          <textarea  class="input input-large" style="width:100%;" rows=13 name=sample_input></textarea><br><br>
+        </p>
+        <p align=left>
+          <?php echo "<h4>".$MSG_Sample_Output."</h4>"?>
+          <textarea  class="input input-large" style="width:100%;" rows=13 name=sample_output></textarea><br><br>
+        </p>
+        <p align=left>
+          <?php echo "<h4>".$MSG_Test_Input."</h4>"?>
+          <?php echo "(".$MSG_HELP_MORE_TESTDATA_LATER.")"?><br>
+          <textarea class="input input-large" style="width:100%;" rows=13 name=test_input></textarea><br><br>
+        </p>
+        <p align=left>
+          <?php echo "<h4>".$MSG_Test_Output."</h4>"?>
+          <?php echo "(".$MSG_HELP_MORE_TESTDATA_LATER.")"?><br>
+          <textarea class="input input-large" style="width:100%;" rows=13 name=test_output></textarea><br><br>
+        </p>
+        <p align=left>
+          <?php echo "<h4>".$MSG_HINT."</h4>"?>
+          <textarea class="kindeditor" rows=13 name=hint cols=80></textarea><br>
+        </p>
+        <p>
+          <?php echo "<h4>".$MSG_SPJ."</h4>"?>
+          <?php echo "(".$MSG_HELP_SPJ.")"?><br>
+          <?php echo "No "?><input type=radio name=spj value='0' checked><?php echo "/ Yes "?><input type=radio name=spj value='1'><br><br>
+        </p>
+        <p align=left>
+          <?php echo "<h4>".$MSG_SOURCE."</h4>"?>
+          <textarea name=source style="width:100%;" rows=1></textarea><br><br>
+        </p>
+        <p align=left><?php echo "<h4>".$MSG_CONTEST."</h4>"?>
+          <select name=contest_id>
+            <?php
+            $sql="SELECT `contest_id`,`title` FROM `contest` WHERE `start_time`>NOW() order by `contest_id`";
+            $result=pdo_query($sql);
+            echo "<option value=''>none</option>";
+            if (count($result)==0) {
+            }
+            else {
+              foreach ($result as $row) {
+                echo "<option value='{$row['contest_id']}'>{$row['contest_id']} {$row['title']}</option>";
+              }
+            }?>
+          </select>
+        </p>
+
+
+        <!-- + by CSL -->
+        <br><br>
+        <p align=left>
+          <?php echo "<h5>"."앞에 붙일 코드"."</h5>"?>
+          <textarea name=front placeholder="제출한 코드의 앞에 붙일 코드" style="width:100%;" rows=1></textarea><br>
+        </p>
+        <p align=left>
+          <?php echo "<h5>"."뒤에 붙일 코드"."</h5>"?>
+          <textarea name=rear placeholder="제출한 코드의 뒤에 붙일 코드" style="width:100%;" rows=1></textarea><br>
+        </p>
+        <p align=left>
+          <?php echo "<h5>"."사용금지 코드"."</h5>"?>
+          <textarea name=bann placeholder="사용을 제한할 단어(/로 구분하여 입력)" style="width:100%;" rows=1></textarea><br>
+        </p>
+        <p align=left>
+          <?php echo "<h5>"."만든사람"."</h5>"?>
+          <textarea name=credits placeholder="한영일, 이일영(역할), ... (2000)" style="width:100%;" rows=1></textarea><br><br>
+        </p>
+        <!-- + by CSL -->
+
+
+        <div align=center>
+          <?php require_once("../include/set_post_key.php");?>
+          <input type=submit value='<?php echo $MSG_SAVE?>' name=submit>
+        </div>
+      </input>
+    </form>
+  </div>
+</body>
+</html>
