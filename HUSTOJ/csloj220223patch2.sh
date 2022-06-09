099e282955ea8818172c0e074c971308ccf02d88
melongist <what_is_computer@msn.com>
2022-06-09 15:23:53 +0900

    22.06.09 update
    22.06.09 update

--- HUSTOJ/csloj220223patch2.sh
+++ HUSTOJ/csloj220223patch2.sh
@@ -17,7 +17,7 @@ fi
 sed -i "s/OJ_USE_DOCKER=1/OJ_USE_DOCKER=0/g" /home/judge/etc/judge.conf
 sed -i "s/OJ_INTERNAL_CLIENT=1/OJ_INTERNAL_CLIENT=0/g" /home/judge/etc/judge.conf
 
-sed -i "s/release 22.02.23p1 /release 22.02.23p2/" /home/judge/src/web/template/bs3/js.php
+sed -i "s/release 22.02.23p1/release 22.02.23p2/" /home/judge/src/web/template/bs3/js.php
 
 echo ""
 echo "---- CSL HUSTOJ pending error patched ----"
