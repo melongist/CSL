cef7545e5ee252ebc1c28805ec0f71aaa90ef843
melongist <what_is_computer@msn.com>
2022-06-09 23:12:53 +0900

    22.06.09 update
    22.06.09 update

--- HUSTOJ/csloj220223patch2.sh
+++ HUSTOJ/csloj220223patch2.sh
@@ -17,6 +17,16 @@ fi
 sed -i "s/OJ_USE_DOCKER=1/OJ_USE_DOCKER=0/g" /home/judge/etc/judge.conf
 sed -i "s/OJ_INTERNAL_CLIENT=1/OJ_INTERNAL_CLIENT=0/g" /home/judge/etc/judge.conf
 
+
+#https server, problem xml, image data including bug fix 
+#problem_export_xml.php customizing for front, rear, bann, credits fields
+wget https://raw.githubusercontent.com/melongist/CSL/master/HUSTOJ/admin/problem_export_xml220223.php
+mv -f ./problem_export_xml220223.php /home/judge/src/web/admin/problem_export_xml.php
+chown www-data:root /home/judge/src/web/admin/problem_export_xml.php
+chmod 664 /home/judge/src/web/admin/problem_export_xml.php
+
+
+
 sed -i "s/release 22.02.23p1/release 22.02.23p2/" /home/judge/src/web/template/bs3/js.php
 
 echo ""
