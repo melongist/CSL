273c9ef29973decea1da46441f553d5a70ca850b
melongist <what_is_computer@msn.com>
2022-06-22 00:03:32 +0900

    22.06.22 update
    22.06.22 update

---
+++ hustoj220621.sh
@@ -0,0 +1,295 @@
+#!/bin/bash
+#Korean HUSTOJ installation script
+#Made by melongist(what_is_computer@msn.com)
+#for Korean
+
+VER_DATE="22.06.21"
+
+THISFILE="hustoj220621.sh"
+SRCZIP="hustoj220621.zip"
+DOCKERFILE="Dockerfile220621"
+
+if [[ -z $SUDO_USER ]] ; then
+  echo "Use 'sudo bash ${THISFILE}'"
+  exit 1
+fi
+
+OSVER=$(grep "Ubuntu" /etc/issue|head -1|awk  '{print $2}')
+
+if [ ${OSVER} != "22.04" ] ; then
+  echo "Ubuntu 22.04 LTS needed!!'"
+  exit 1
+fi
+
+
+#detect and refuse to run under WSL
+if [ -d /mnt/c ]; then
+    echo "WSL is NOT supported."
+    exit 1
+fi
+
+cd
+
+#for OJ NAME
+clear
+OJNAME="o"
+INPUTS="x"
+while [ ${OJNAME} != ${INPUTS} ]; do
+  echo -n "Enter  OJ NAME : "
+  read OJNAME
+  echo -n "Repeat OJ NAME : "
+  read INPUTS
+done
+
+#for South Korea's timezone
+timedatectl set-timezone 'Asia/Seoul'
+
+apt update
+apt -y upgrade
+apt -y autoremove
+
+apt install -y software-properties-common
+add-apt-repository -y universe
+add-apt-repository -y multiverse
+add-apt-repository -y restricted
+
+apt -y install subversion zip unzip curl
+
+/usr/sbin/useradd -m -u 1536 judge
+
+cd /home/judge/ || exit
+
+#using tgz src files
+#wget -O hustoj.tar.gz http://dl.hustoj.com/hustoj.tar.gz
+#tar xzf hustoj.tar.gz
+#svn up src
+#svn co https://github.com/zhblue/hustoj/trunk/trunk/  src
+
+#how to make src zip
+#zip -r hustojYYMMDD.zip ./src
+wget https://raw.githubusercontent.com/melongist/CSL/master/HUSTOJ/${SRCZIP}
+unzip ${SRCZIP}
+rm ${SRCZIP}
+
+#changing Dockerfile
+wget https://raw.githubusercontent.com/melongist/CSL/master/HUSTOJ/${DOCKERFILE}
+chown root:root ./${DOCKERFILE}
+chmod 644 ./${DOCKERFILE}
+mv -f ./${DOCKERFILE} /home/judge/src/install/Dockerfile
+
+
+#------ original intallation scripts start
+
+
+
+
+#手工解决阿里云软件源的包依赖问题
+apt install libssl1.1=1.1.1f-1ubuntu2.8 -y --allow-downgrades
+apt-get install -y libmysqlclient-dev
+apt-get install -y libmysql++-dev 
+PHP_VER=`apt-cache search php-fpm|grep -e '[[:digit:]]\.[[:digit:]]' -o`
+if [ "$PHP_VER" = "" ] ; then PHP_VER="8.1"; fi
+for pkg in net-tools make g++ php$PHP_VER-fpm nginx mysql-server php$PHP_VER-mysql php$PHP_VER-common php$PHP_VER-gd php$PHP_VER-zip php$PHP_VER-mbstring php$PHP_VER-xml php$PHP_VER-curl php$PHP_VER-intl php$PHP_VER-xmlrpc php$PHP_VER-soap tzdata
+do
+  while ! apt-get install -y "$pkg" 
+  do
+    echo "Network fail, retry... you might want to change another apt source for install"
+  done
+done
+
+chgrp www-data  /home/judge
+
+USER=$(grep user /etc/mysql/debian.cnf|head -1|awk  '{print $3}')
+PASSWORD=$(grep password /etc/mysql/debian.cnf|head -1|awk  '{print $3}')
+CPU=$(grep "cpu cores" /proc/cpuinfo |head -1|awk '{print $4}')
+
+mkdir etc data log backup
+
+cp src/install/java0.policy  /home/judge/etc
+cp src/install/judge.conf  /home/judge/etc
+chmod +x src/install/ans2out
+
+# create enough runX dirs for each CPU core
+if grep "OJ_SHM_RUN=0" etc/judge.conf ; then
+  for N in `seq 0 $(($CPU-1))`
+  do
+     mkdir run$N
+     chown judge run$N
+  done
+fi
+
+sed -i "s/OJ_USER_NAME=root/OJ_USER_NAME=$USER/g" etc/judge.conf
+sed -i "s/OJ_PASSWORD=root/OJ_PASSWORD=$PASSWORD/g" etc/judge.conf
+sed -i "s/OJ_COMPILE_CHROOT=1/OJ_COMPILE_CHROOT=0/g" etc/judge.conf
+sed -i "s/OJ_RUNNING=1/OJ_RUNNING=$CPU/g" etc/judge.conf
+
+chmod 700 backup
+chmod 700 etc/judge.conf
+
+sed -i "s/DB_USER[[:space:]]*=[[:space:]]*\"root\"/DB_USER=\"$USER\"/g" src/web/include/db_info.inc.php
+sed -i "s/DB_PASS[[:space:]]*=[[:space:]]*\"root\"/DB_PASS=\"$PASSWORD\"/g" src/web/include/db_info.inc.php
+chmod 700 src/web/include/db_info.inc.php
+chown -R www-data src/web/
+
+chown -R root:root src/web/.svn
+chmod 750 -R src/web/.svn
+
+chown www-data:judge src/web/upload
+chown www-data:judge data
+chmod 711 -R data
+if grep "client_max_body_size" /etc/nginx/nginx.conf ; then 
+  echo "client_max_body_size already added" ;
+else
+  sed -i "s:include /etc/nginx/mime.types;:client_max_body_size    80m;\n\tinclude /etc/nginx/mime.types;:g" /etc/nginx/nginx.conf
+fi
+
+mysql -h localhost -u"$USER" -p"$PASSWORD" < src/install/db.sql
+echo "insert into jol.privilege values('admin','administrator','true','N');"|mysql -h localhost -u"$USER" -p"$PASSWORD" 
+
+if grep "added by hustoj" /etc/nginx/sites-enabled/default ; then
+  echo "default site modified!"
+else
+  echo "modify the default site"
+  sed -i "s#root /var/www/html;#root /home/judge/src/web;#g" /etc/nginx/sites-enabled/default
+  sed -i "s:index index.html:index index.php:g" /etc/nginx/sites-enabled/default
+  sed -i "s:#location ~ \\\.php\\$:location ~ \\\.php\\$:g" /etc/nginx/sites-enabled/default
+  sed -i "s:#\tinclude snippets:\tinclude snippets:g" /etc/nginx/sites-enabled/default
+  sed -i "s|#\tfastcgi_pass unix|\tfastcgi_pass unix|g" /etc/nginx/sites-enabled/default
+  sed -i "s:}#added by hustoj::g" /etc/nginx/sites-enabled/default
+  sed -i "s:php7.4:php$PHP_VER:g" /etc/nginx/sites-enabled/default
+  sed -i "s|# deny access to .htaccess files|}#added by hustoj\n\n\n\t# deny access to .htaccess files|g" /etc/nginx/sites-enabled/default
+fi
+/etc/init.d/nginx restart
+sed -i "s/post_max_size = 8M/post_max_size = 80M/g" /etc/php/$PHP_VER/fpm/php.ini
+sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 80M/g" /etc/php/$PHP_VER/fpm/php.ini
+WWW_CONF=$(find /etc/php -name www.conf)
+sed -i 's/;request_terminate_timeout = 0/request_terminate_timeout = 128/g' "$WWW_CONF"
+sed -i 's/pm.max_children = 5/pm.max_children = 200/g' "$WWW_CONF"
+ 
+COMPENSATION=$(grep 'mips' /proc/cpuinfo|head -1|awk -F: '{printf("%.2f",$2/5000)}')
+sed -i "s/OJ_CPU_COMPENSATION=1.0/OJ_CPU_COMPENSATION=$COMPENSATION/g" etc/judge.conf
+
+PHP_FPM=$(find /etc/init.d/ -name "php*-fpm")
+$PHP_FPM restart
+PHP_FPM=$(service --status-all|grep php|awk '{print $4}')
+if [ "$PHP_FPM" != ""  ]; then service "$PHP_FPM" restart ;else echo "NO PHP FPM";fi;
+
+cd src/core || exit 
+chmod +x ./make.sh
+./make.sh
+if grep "/usr/bin/judged" /etc/rc.local ; then
+  echo "auto start judged added!"
+else
+  sed -i "s/exit 0//g" /etc/rc.local
+  echo "/usr/bin/judged" >> /etc/rc.local
+  echo "exit 0" >> /etc/rc.local
+fi
+if grep "bak.sh" /var/spool/cron/crontabs/root ; then
+  echo "auto backup added!"
+else
+  crontab -l > conf && echo "1 0 * * * /home/judge/src/install/bak.sh" >> conf && crontab conf && rm -f conf
+fi
+ln -s /usr/bin/mcs /usr/bin/gmcs
+
+/usr/bin/judged
+cp /home/judge/src/install/hustoj /etc/init.d/hustoj
+update-rc.d hustoj defaults
+systemctl enable hustoj
+systemctl enable nginx
+systemctl enable mysql
+systemctl enable php$PHP_VER-fpm
+#systemctl enable judged
+
+sed -i "s#interactive_timeout=120#interactive_timeout=20#g" /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i "s#wait_timeout=120#wait_timeout=20#g" /etc/mysql/mysql.conf.d/mysqld.cnf
+
+/etc/init.d/mysql start
+
+
+mkdir /var/log/hustoj/
+chown www-data -R /var/log/hustoj/
+cd /home/judge/src/install
+if test -f  /.dockerenv ;then
+  echo "Already in docker, skip docker installation, install some compilers ... "
+  apt-get intall -y flex fp-compiler openjdk-14-jdk mono-devel
+else
+  bash docker.sh
+   sed -i "s/OJ_USE_DOCKER=0/OJ_USE_DOCKER=1/g" /home/judge/etc/judge.conf
+   sed -i "s/OJ_PYTHON_FREE=0/OJ_PYTHON_FREE=1/g" /home/judge/etc/judge.conf
+fi
+
+#------ original intallation scripts end
+
+#cls
+#reset
+
+
+
+
+cd
+
+#judge.conf edit
+#time result fix ... for use_max_time : to record the max time of all results, not sum of...
+sed -i "s/OJ_USE_MAX_TIME=0/OJ_USE_MAX_TIME=1/" /home/judge/etc/judge.conf
+sed -i "s/OJ_TIME_LIMIT_TO_TOTAL=1/OJ_TIME_LIMIT_TO_TOTAL=0/" /home/judge/etc/judge.conf
+
+#db_info.inc.php edit
+#for OJ name
+sed -i "s/OJ_NAME=\"HUSTOJ\"/OJ_NAME=\"${OJNAME}\"/" /home/judge/src/web/include/db_info.inc.php
+#for bs3 template
+sed -i "s/OJ_TEMPLATE=\"syzoj\"/OJ_TEMPLATE=\"bs3\"/" /home/judge/src/web/include/db_info.inc.php
+
+#for korean kindeditor
+sed -i "s/OJ_LANG=\"en\"/OJ_LANG=\"ko\"/" /home/judge/src/web/include/db_info.inc.php
+sed -i "s/zh_CN.js/ko.js/" /home/judge/src/web/admin/kindeditor.php
+
+#Removing QR codes + CSL
+wget https://raw.githubusercontent.com/melongist/CSL/master/HUSTOJ/js220621.php
+mv -f ./js220621.php /home/judge/src/web/template/bs3/js.php
+chown www-data:${SUDO_USER} /home/judge/src/web/template/bs3/js.php
+chmod 664 /home/judge/src/web/template/bs3/js.php
+sed -i "s/release YY.MM.DD/release ${VER_DATE}/" /home/judge/src/web/template/bs3/js.php
+
+
+#Identifing AWS Ubuntu 22.04 LTS
+if [ -f /etc/default/grub.d/50-cloudimg-settings.cfg ]; then
+  SERVERTYPES="AWS SERVER"
+  IPADDRESS=($(curl http://checkip.amazonaws.com))
+else
+  SERVERTYPES="LOCAL SERVER"
+  IPADDRESS=($(hostname -I))
+fi
+
+
+#temporary fix until next release
+#...
+#sed -i "s/<img src=\"refresh/<\!--<img src=\"refresh/" /home/judge/src/web/template/bs3/error.php
+#sed -i "s/on error\" >/on error\" >-->/" /home/judge/src/web/template/bs3/error.php
+mkdir /home/judge/src/web/admin/msg
+chown www-data:root /home/judge/src/web/admin/msg
+chmod 744 /home/judge/src/web/admin/msg
+
+#sed -i "s/style=\"position:fixed;z-index:9999;width:100%;margin-bottom:50px\"//" /home/judge/src/web/template/bs3/nav.php
+
+#Replacing msg.txt
+wget https://raw.githubusercontent.com/melongist/CSL/master/HUSTOJ/msg220621.txt
+mv -f ./msg220621.txt /home/judge/src/web/admin/msg/${IPADDRESS[0]}.txt
+chown www-data:${SUDO_USER} /home/judge/src/web/admin/msg/${IPADDRESS[0]}.txt
+chmod 644 /home/judge/src/web/admin/msg/${IPADDRESS[0]}.txt
+sed -i "s/release YY.MM.DD/release ${VER_DATE}/" /home/judge/src/web/admin/msg/${IPADDRESS[0]}.txt
+
+
+#clear
+
+echo ""
+echo "--- $OJNAME HUSTOJ installed!! ---"
+echo ""
+echo "Register admin!"
+echo ""
+echo "$SERVERTYPES"
+echo "http://${IPADDRESS[0]}"
+echo ""
+echo ""
+echo "Check & Edit HUSTOJ configurations"
+echo "sudo vi /home/judge/src/web/include/db_info.inc.php"
+echo ""
