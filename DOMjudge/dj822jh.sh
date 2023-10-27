cf5627665a19303e8d80190635540302737b4a24
Hyun-seok Jeon <what_is_computer@msn.com>
2023-10-28 06:50:52 +0900

    23.10.28 update
    23.10.28 update

---
+++ DOMjudge/dj822jh.sh
@@ -0,0 +1,170 @@
+#!/bin/bash
+
+#origin
+#https://www.domjudge.org/
+#https://github.com/DOMjudge/domjudge
+
+#DOMjudge server installation script
+#DOMjudge8.2.2 stable + Ubuntu 22.04.3 LTS + apache2 2.4.52
+#2023.11 Made by melongist(melongist@gmail.com) for CS teachers
+
+#terminal commands to install DOMjudge judgehosts
+#------
+#wget https://raw.githubusercontent.com/melongist/CSL/master/DOMjudge/dj822jh.sh
+#bash dj822jh.sh
+
+
+if [[ $SUDO_USER ]] ; then
+  echo "Just use 'bash dj822jh.sh'"
+  exit 1
+fi
+
+cd
+
+sudo apt update
+sudo apt -y upgrade
+
+sudo apt -y install make
+sudo apt -y install pkg-config
+sudo apt -y install sudo
+sudo apt -y install debootstrap
+sudo apt -y install libcgroup-dev
+sudo apt -y install php8.2-cli
+sudo apt -y install php8.2-curl
+sudo apt -y install php8.2-xml
+sudo apt -y install php8.2-zip
+sudo apt -y install lsof
+sudo apt -y install procps
+
+#for ubuntu
+sudo apt -y remove apport
+
+
+
+
+
+
+#pypy3
+sudo apt -y install pypy3
+#java
+sudo apt -y install default-jre-headless
+sudo apt -y install default-jdk-headless
+#haskell
+sudo apt -y install ghc
+#pascal
+sudo apt -y install fp-compiler
+#JavaScript
+sudo apt -y install nodejs
+sudo apt -y install npm
+#R
+sudo apt -y install r-base
+#rust
+sudo apt -y install rustc
+
+
+
+
+#Register DOMjudge memory autoscaling for php(fpm)
+sudo rm /etc/rc.local
+sudo touch /etc/rc.local
+sudo chmod 777 /etc/rc.local
+if grep "/home/ubuntu/dj822mas.sh" /etc/rc.local ; then
+  echo "DOMjudge memory autoscaling for php(fpm) registered!"
+else
+  sudo sed -i "s/exit 0//g" /etc/rc.local
+  sudo echo "#! /bin/sh" >> /etc/rc.local
+  sudo echo "bash /home/ubuntu/dj822mas.sh" >> /etc/rc.local
+  sudo echo "exit 0" >> /etc/rc.local
+fi
+sudo chmod 755 /etc/rc.local
+
+
+
+
+#Building and installing
+cd domjudge-8.2.2
+./configure --prefix=/opt/domjudge --with-baseurl=BASEURL
+make judgehost
+sudo make install-judgehost
+echo ""
+
+
+
+
+#Judgehosts group and user
+#multiple judgedaemons per machine
+#checking the number of CPU(s)
+lscpu | grep "^CPU(s)"
+CPUS=$(lscpu | grep "^CPU(s)"|awk  '{print $2}')
+#make judgedaemo group
+sudo groupadd domjudge-run
+#default judgedaemon
+sudo useradd -d /nonexistent -g domjudge-run -M -s /bin/false domjudge-run
+#multi judgedaemons, max 127
+for ((i=1; i<=127; i++));
+do
+  sudo useradd -d /nonexistent -g domjudge-run -M -s /bin/false domjudge-run-$i
+done
+
+
+
+
+#Sudo permissions
+sudo cp /opt/domjudge/judgehost/etc/sudoers-domjudge /etc/sudoers.d/
+sudo chmod 0440 /etc/sudoers.d/sudoers-domjudge
+
+
+
+
+#Creating a chroot environment
+#make chroot
+#add nodejs, r-base
+#sudo sed -i "s#INSTALLDEBS=\"gcc g++ make default-jdk-headless default-jre-headless pypy3 locales\"#INSTALLDEBS=\"gcc g++ make default-jdk-headless default-jre-headless pypy3 nodejs r-base locales\"#" /opt/domjudge/judgehost/bin/dj_make_chroot
+#default
+sudo /opt/domjudge/judgehost/bin/dj_make_chroot -i nodejs,r-base,rustc
+
+
+
+
+#Linux Control Groups
+#try #1 for Ubuntu 22.04 LTS
+sudo sed -i "s#GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash\"#GRUB_CMDLINE_LINUX_DEFAULT=\"quiet cgroup_enable=memory swapaccount=1 isolcpus=2 systemd.unified_cgroup_hierarchy=0\"#" /etc/default/grub
+#try #2 AWS Ubuntu 22.04 LTS Server
+if [ -f /etc/default/grub.d/50-cloudimg-settings.cfg ]; then
+	echo "Editing /etc/default/grub.d/50-cloudimg-settings.cfg for AWS"
+  sudo sed -i "s#GRUB_CMDLINE_LINUX_DEFAULT=\"console=tty1 console=ttyS0 nvme_core.io_timeout=4294967295\"#GRUB_CMDLINE_LINUX_DEFAULT=\"console=tty1 console=ttyS0 nvme_core.io_timeout=4294967295 quiet cgroup_enable=memory swapaccount=1 isolcpus=2 systemd.unified_cgroup_hierarchy=0\"#" /etc/default/grub.d/50-cloudimg-settings.cfg
+fi
+sudo update-grub
+#sudo systemctl enable create-cgroups --now
+
+
+
+
+#make docs
+sudo apt -y install python3-sphinx python3-sphinx-rtd-theme rst2pdf fontconfig python3-yaml texlive-latex-extra latexmk
+
+sudo mkdir /opt/domjudge/doc
+sudo mkdir /opt/domjudge/doc/manual
+sudo mkdir /opt/domjudge/doc/manual/html
+sudo mkdir /opt/domjudge/doc/examples
+sudo mkdir /opt/domjudge/doc/logos
+
+cd ~/domjudge-8.2.2/doc/
+
+make docs
+
+sudo make install-docs
+
+
+
+#swift not working... temporary removed. 2022.11.17
+#sudo apt -y install clang libicu-dev
+#sudo wget https://download.swift.org/swift-5.7.1-release/ubuntu2204/swift-5.7.1-RELEASE/swift-5.7.1-RELEASE-ubuntu22.04.tar.gz
+#sudo tar -zxvf swift-5.7.1-RELEASE-ubuntu22.04.tar.gz
+#sudo rm swift-5.7.1-RELEASE-ubuntu22.04.tar.gz
+#sudo mv ~/swift-5.7.1-RELEASE-ubuntu22.04 ~/swift
+
+#sudo ln -s -f ~/swift/usr/bin/swiftc /usr/bin/swiftc
+
+
+sudo apt -y autoremove
---
+++ DOMjudge/dj822kr.sh
@@ -0,0 +1,176 @@
+#!/bin/bash
+
+#DOMjudge server Korean language patch script
+#2023.11 Made by melongist(melongist@gmail.com) for CS teachers
+#DOMjudge8.2.2 stable + Ubuntu 22.04.3 LTS + apache2 2.4.52
+
+#terminal commands to install Korean patch 
+#------
+#wget https://raw.githubusercontent.com/melongist/CSL/master/DOMjudge/dj822kr.sh
+#bash dj822kr.sh
+
+
+if [[ $SUDO_USER ]] ; then
+  echo "Just use 'bash dj822kr.sh'"
+  exit 1
+fi
+
+clear
+
+INPUTS="x"
+
+while [ ${INPUTS} != "y" ] && [ ${INPUTS} != "n" ]; do
+  echo -n "Rename DOMjudge logo name? [y/n]: "
+  read INPUTS
+done
+
+echo ""
+if [ ${INPUTS} == "y" ] ; then
+  OJNAME="o"
+  INPUTS="x"
+  while [ ${OJNAME} != ${INPUTS} ]; do
+    echo -n "Enter  DOMjudge NAME : "
+    read OJNAME
+    echo -n "Repeat DOMjudge NAME : "
+    read INPUTS
+  done
+else
+  OJNAME="DOMjudge"
+fi
+
+echo ""
+
+#menu
+sudo sed -i "s/DOMjudge/${OJNAME}/" /opt/domjudge/domserver/webapp/templates/public/menu.html.twig
+sudo sed -i "s/Scoreboard/점수/" /opt/domjudge/domserver/webapp/templates/public/menu.html.twig
+sudo sed -i "s/Problemset/문제/" /opt/domjudge/domserver/webapp/templates/public/menu.html.twig
+sudo sed -i "s/Team/팀/" /opt/domjudge/domserver/webapp/templates/public/menu.html.twig
+sudo sed -i "s/Jury/대회운영/" /opt/domjudge/domserver/webapp/templates/public/menu.html.twig
+
+#login
+sudo sed -i "s/Please sign in/로그인하세요./" /opt/domjudge/domserver/webapp/templates/security/login.html.twig
+sudo sed -i "s/\"submit\">Sign in/\"submit\">로그인/" /opt/domjudge/domserver/webapp/templates/security/login.html.twig
+
+#login/logout button
+sudo sed -i "s/i> Logout/i> 로그아웃/" /opt/domjudge/domserver/webapp/templates/partials/menu_login_logout_button.html.twig
+sudo sed -i "s/i> Login/i> 로그인/" /opt/domjudge/domserver/webapp/templates/partials/menu_login_logout_button.html.twig
+
+#change_contest
+sudo sed -i "s/>Change Contest/>대회 변경/" /opt/domjudge/domserver/webapp/templates/partials/menu_change_contest.html.twig
+sudo sed -i "s/>no contest/>대회 없음/" /opt/domjudge/domserver/webapp/templates/partials/menu_change_contest.html.twig
+
+#countdown
+sudo sed -i "s/no contest/대회 없음/" /opt/domjudge/domserver/webapp/templates/partials/menu_countdown.html.twig
+
+#problem list
+sudo sed -i "s/problems</문제</" /opt/domjudge/domserver/webapp/templates/partials/problem_list.html.twig
+
+#scoreboard
+sudo sed -i "s/No active contest/진행중인 대회 없음/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard.html.twig
+sudo sed -i "s/preliminary results - not final/가채점 결과 - 최종 결과 아님/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard.html.twig
+sudo sed -i "s/final standings/최종 순위/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard.html.twig
+sudo sed -i "s/contest over, waiting for results/대회 종료됨, 최종 결과 확인중/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard.html.twig
+sudo sed -i "s/starts:/시작시간:/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard.html.twig
+sudo sed -i "s/started:/시작시간:/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard.html.twig
+sudo sed -i "s/- ends:/- 종료시간:/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard.html.twig
+sudo sed -i "s/The scoreboard was frozen with/대회 종료/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard.html.twig
+sudo sed -i "s/minutes remaining - solutions/분전에 스코어보드가 변하지 않게 고정(frozen)되었습니다. -/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard.html.twig
+sudo sed -i "s/submitted in the last/대회 종료/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard.html.twig
+sudo sed -i "s/minutes of the contest/분전 스코어보드 고정(frozen) 이후에 제출된 것들은/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard.html.twig
+sudo sed -i "s/are still shown as pending/제출된 것으로만 보입니다./" /opt/domjudge/domserver/webapp/templates/partials/scoreboard.html.twig
+sudo sed -i "s/are not shown/보이지 않습니다./" /opt/domjudge/domserver/webapp/templates/partials/scoreboard.html.twig
+
+#scoreboard summary
+sudo sed -i "s/>Summary/>통계/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard_summary.html.twig
+sudo sed -i "s/total solved/맞은 개수/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard_summary.html.twig
+sudo sed -i "s/number of accepted submissions/맞음/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard_summary.html.twig
+sudo sed -i "s/number of rejected submissions/틀림/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard_summary.html.twig
+sudo sed -i "s/number of pending submissions/제출됨/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard_summary.html.twig
+sudo sed -i "s/first solved/처음 맞은 시간/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard_summary.html.twig
+sudo sed -i "s/min/분/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard_summary.html.twig
+
+#scoreboard table
+sudo sed -i "s/\"rank/\"순위/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard_table.html.twig
+sudo sed -i "s/>rank/>순위/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard_table.html.twig
+sudo sed -i "s/\"team name/\"팀 이름/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard_table.html.twig
+sudo sed -i "s/>team/>팀 이름/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard_table.html.twig
+sudo sed -i "s/solved \/ penalty time/맞음 \/ 시간 패널티/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard_table.html.twig
+sudo sed -i "s/>score/>점수/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard_table.html.twig
+sudo sed -i "s/Solved first/가장 먼저 맞음/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard_table.html.twig
+sudo sed -i "s/Solved/맞음/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard_table.html.twig
+sudo sed -i "s/Tried, incorrect/틀림/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard_table.html.twig
+sudo sed -i "s/Tried, pending/채점 제출됨/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard_table.html.twig
+sudo sed -i "s/Untried/미제출/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard_table.html.twig
+sudo sed -i "s/Cell colours/색상별 의미/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard_table.html.twig
+sudo sed -i "s/>Categories/>구분/" /opt/domjudge/domserver/webapp/templates/partials/scoreboard_table.html.twig
+
+#team menu
+sudo sed -i "s/DOMjudge/${OJNAME}/" /opt/domjudge/domserver/webapp/templates/team/menu.html.twig
+sudo sed -i "s/> Home/> 처음화면/" /opt/domjudge/domserver/webapp/templates/team/menu.html.twig
+sudo sed -i "s/> Problemset/> 문제/" /opt/domjudge/domserver/webapp/templates/team/menu.html.twig
+sudo sed -i "s/> Print/> 인쇄/" /opt/domjudge/domserver/webapp/templates/team/menu.html.twig
+sudo sed -i "s/> Scoreboard/> 순위/" /opt/domjudge/domserver/webapp/templates/team/menu.html.twig
+sudo sed -i "s/> Jury/> 대회운영/" /opt/domjudge/domserver/webapp/templates/team/menu.html.twig
+sudo sed -i "s/> Submit/> 채점 제출/" /opt/domjudge/domserver/webapp/templates/team/menu.html.twig
+
+#team submit modal
+sudo sed -i "s/> Submit/> 채점 제출/" /opt/domjudge/domserver/webapp/templates/team/submit_modal.html.twig
+sudo sed -i "s/Contest has not yet started - cannot submit./대회가 아직 시작되지 않았습니다. - 제출할 수 없습니다./" /opt/domjudge/domserver/webapp/templates/team/submit_modal.html.twig
+sudo sed -i "s/>Close/>닫기/" /opt/domjudge/domserver/webapp/templates/team/submit_modal.html.twig
+sudo sed -i "s/>Cancel/>취소/" /opt/domjudge/domserver/webapp/templates/team/submit_modal.html.twig
+
+#team clarification add modal
+sudo sed -i "s/Send clarification request/명확한 설명 요청/" /opt/domjudge/domserver/webapp/templates/team/clarification_add_modal.html.twig
+sudo sed -i "s/>Cancel/>취소/" /opt/domjudge/domserver/webapp/templates/team/clarification_add_modal.html.twig
+sudo sed -i "s/> Send/> 보내기/" /opt/domjudge/domserver/webapp/templates/team/clarification_add_modal.html.twig
+
+#team partials index_content
+sudo sed -i "s/Welcome team/환영합니다\!/" /opt/domjudge/domserver/webapp/templates/team/partials/index_content.html.twig
+sudo sed -i "s/There's no active contest for you (yet)./현재 진행중인 대회가 없습니다./" /opt/domjudge/domserver/webapp/templates/team/partials/index_content.html.twig
+sudo sed -i "s/Contest {/대회 {/" /opt/domjudge/domserver/webapp/templates/team/partials/index_content.html.twig
+sudo sed -i "s/>Submissions/>채점 제출 기록/" /opt/domjudge/domserver/webapp/templates/team/partials/index_content.html.twig
+sudo sed -i "s/>Clarifications/>공지 및 안내/" /opt/domjudge/domserver/webapp/templates/team/partials/index_content.html.twig
+sudo sed -i "s/>No clarifications./>없음/" /opt/domjudge/domserver/webapp/templates/team/partials/index_content.html.twig
+sudo sed -i "s/>Clarification Requests/>명확한 설명 요청하기/" /opt/domjudge/domserver/webapp/templates/team/partials/index_content.html.twig
+sudo sed -i "s/>No clarification request./>없음/" /opt/domjudge/domserver/webapp/templates/team/partials/index_content.html.twig
+sudo sed -i "s/request clarification/요청하기/" /opt/domjudge/domserver/webapp/templates/team/partials/index_content.html.twig
+
+#team submission list
+sudo sed -i "s/>No submissions/>제출 기록 없음/" /opt/domjudge/domserver/webapp/templates/team/partials/submission_list.html.twig
+sudo sed -i "s/>time/>제출 시간/" /opt/domjudge/domserver/webapp/templates/team/partials/submission_list.html.twig
+sudo sed -i "s/>problem/>문제/" /opt/domjudge/domserver/webapp/templates/team/partials/submission_list.html.twig
+sudo sed -i "s/>lang/>제출 언어/" /opt/domjudge/domserver/webapp/templates/team/partials/submission_list.html.twig
+sudo sed -i "s/>result/>채점 결과/" /opt/domjudge/domserver/webapp/templates/team/partials/submission_list.html.twig
+
+#team clarification list
+sudo sed -i "s/>time/>시간/" /opt/domjudge/domserver/webapp/templates/team/partials/clarification_list.html.twig
+sudo sed -i "s/>from/>보낸 사람/" /opt/domjudge/domserver/webapp/templates/team/partials/clarification_list.html.twig
+sudo sed -i "s/>to/>받는 사람/" /opt/domjudge/domserver/webapp/templates/team/partials/clarification_list.html.twig
+sudo sed -i "s/>subject/>제목/" /opt/domjudge/domserver/webapp/templates/team/partials/clarification_list.html.twig
+sudo sed -i "s/>text/>내용/" /opt/domjudge/domserver/webapp/templates/team/partials/clarification_list.html.twig
+
+#team submit scripts
+sudo sed -i "s/Main source file:/소스 파일:/" /opt/domjudge/domserver/webapp/templates/team/partials/submit_scripts.html.twig
+sudo sed -i "s/Problem:/문제:/" /opt/domjudge/domserver/webapp/templates/team/partials/submit_scripts.html.twig
+sudo sed -i "s/Language:/프로그래밍언어:/" /opt/domjudge/domserver/webapp/templates/team/partials/submit_scripts.html.twig
+sudo sed -i "s/Make submission\?/제출하시겠습니까\?/" /opt/domjudge/domserver/webapp/templates/team/partials/submit_scripts.html.twig
+
+#etc scripts
+sudo sed -i "s/Submission done\! Watch for the verdict in the list below./채점이 제출되었습니다\! 페이지 새로고침을 눌러 채점 결과를 확인해주세요./" /opt/domjudge/domserver/webapp/src/Controller/Team/SubmissionController.php
+sudo sed -i "s/No file selected/파일 선택/" /opt/domjudge/domserver/webapp/templates/form_theme.html.twig
+sudo sed -i "s/Select a problem/문제 선택/" /opt/domjudge/domserver/webapp/src/Form/Type/SubmitProblemType.php
+sudo sed -i "s/Select a language/프로그래밍언어 선택/" /opt/domjudge/domserver/webapp/src/Form/Type/SubmitProblemType.php
+
+
+
+#clearing DOMjudge webserver cache
+sudo rm -rf /opt/domjudge/domserver/webapp/var/cache/prod/*
+
+echo "" | tee -a ~/domjudge.txt
+echo "DOMjudge 8.2.2 stable 23.10.20" | tee -a ~/domjudge.txt
+echo "DOMjudge participants' korean interface installed!" | tee -a ~/domjudge.txt
+echo "For korean middle & high school students." | tee -a ~/domjudge.txt
+echo "" | tee -a ~/domjudge.txt
+echo "Check DOMserver's web interface!" | tee -a ~/domjudge.txt
+echo "------" | tee -a ~/domjudge.txt
+echo "http://localhost/domjudge/" | tee -a ~/domjudge.txt
