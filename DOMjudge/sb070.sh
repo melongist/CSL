6e5761e9a52a06b17a7b33f4d35d8b75015bba6e
Hyun-seok Jeon <what_is_computer@msn.com>
2024-01-14 11:11:08 +0900

    24.01.14 update
    24.01.14 update

--- DOMjudge/sb070.sh
+++ DOMjudge/sb070.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+#origin
 #https://github.com/spotboard
 #https://github.com/spotboard/spotboard
 
 #DOMjudge judgehost starting script
-#DOMjudge8.1.2 stable + Ubuntu 22.04 LTS
-#Made by 
-#2022.08.04 melongist(melongist@gmail.com, what_is_computer@msn.com) for CS teachers
+#2024.01 Made by melongist(melongist@gmail.com) for CS teachers
+#DOMjudge8.2.2 stable + Ubuntu 22.04.3 LTS + apache2/nginx
 
 
-#spotboard 0.7.0 for domjudge8.1.2 + Ubuntu 22.04 LTS Server
+#spotboard 0.7.0 for domjudge8.2.2 + Ubuntu 22.04 LTS Server
 
 #terminal commands to install spotboard webapp
+#------
 #wget https://raw.githubusercontent.com/melongist/CSL/master/DOMjudge/sb070.sh
 #bash sb070.sh
 
@@ -28,12 +29,34 @@ cd
 sudo apt update
 sudo apt -y upgrade
 
+
+WEBSERVER="no"
+while [ ${WEBSERVER} != "apache2" ] && [ ${WEBSERVER} != "nginx" ]; do
+  clear
+  echo    ""
+  echo    "Select Web-server for DOMjudge!"
+  echo -n "apache2 or nginx? [apache2/nginx]: "
+  read WEBSERVER
+done
+
+
 #https://github.com/spotboard/spotboard
 wget https://raw.githubusercontent.com/melongist/CSL/master/DOMjudge/spotboard-webapp-0.7.0.tar.gz
 tar -xvf spotboard-webapp-0.7.0.tar.gz
 mv spotboard-webapp-0.7.0 spotboard
-sudo mv spotboard /var/www/html/
-cd /var/www/html/spotboard/
+
+
+case ${WEBSERVER} in
+  "apache2")
+    sudo mv spotboard /var/www/html/
+    cd /var/www/html/spotboard/
+    ;;
+  "nginx")
+    sudo mv spotboard /usr/share/nginx/html/
+    cd /usr/share/nginx/html/spotboard/
+    ;;
+esac
+
 
 sudo apt install -y curl
 
@@ -64,12 +87,18 @@ npm -v
 npm run build
 
 
-sed -i "s#feed_server_path = './sample/'#feed_server_path = './'#" /var/www/html/spotboard/dist/config.js
-sed -i "s#'award_slide.json' :'./sample/award_slide.json'#'award_slide.json' :'./award_slide.json'#" /var/www/html/spotboard/dist/config.js
-sed -i "s#animation          : false#animation          : true#" /var/www/html/spotboard/dist/config.js
-
-
-
+case ${WEBSERVER} in
+  "apache2")
+    sed -i "s#feed_server_path = './sample/'#feed_server_path = './'#" /var/www/html/spotboard/dist/config.js
+    sed -i "s#'award_slide.json' :'./sample/award_slide.json'#'award_slide.json' :'./award_slide.json'#" /var/www/html/spotboard/dist/config.js
+    sed -i "s#animation          : false#animation          : true#" /var/www/html/spotboard/dist/config.js
+    ;;
+  "nginx")
+    sed -i "s#feed_server_path = './sample/'#feed_server_path = './'#" /usr/share/nginx/html/spotboard/dist/config.js
+    sed -i "s#'award_slide.json' :'./sample/award_slide.json'#'award_slide.json' :'./award_slide.json'#" /usr/share/nginx/html/spotboard/dist/config.js
+    sed -i "s#animation          : false#animation          : true#" /usr/share/nginx/html/spotboard/dist/config.js
+    ;;
+esac
 
 
 cd
@@ -78,17 +107,25 @@ clear
 
 echo "" | tee -a ~/domjudge.txt
 echo "spotboard for domjudge installed!" | tee -a ~/domjudge.txt
-echo "Ver 2022.08.04" | tee -a ~/domjudge.txt
+echo "Ver 2024.01.14" | tee -a ~/domjudge.txt
 echo "" | tee -a ~/domjudge.txt
 echo "Check spotboard!" | tee -a ~/domjudge.txt
 echo "------" | tee -a ~/domjudge.txt
 echo "http://localhost/spotboard/dist/" | tee -a ~/domjudge.txt
 echo "" | tee -a ~/domjudge.txt
 echo "configuration for spotboard" | tee -a ~/domjudge.txt
-echo "check /var/www/html/spotboard/dist/config.js" | tee -a ~/domjudge.txt
+
+case ${WEBSERVER} in
+  "apache2")
+    echo "check /var/www/html/spotboard/dist/config.js" | tee -a ~/domjudge.txt
+    ;;
+  "nginx")
+    echo "check /usr/share/nginx/html/spotboard/dist/config.js" | tee -a ~/domjudge.txt
+    ;;
+esac
+
 echo "" | tee -a ~/domjudge.txt
 echo "Next step : install domjudge-converter" | tee -a ~/domjudge.txt
 echo "" | tee -a ~/domjudge.txt
 echo "" | tee -a ~/domjudge.txt
-
 
