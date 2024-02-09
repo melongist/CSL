437da9f5771e6b3be0daa4d5ca2338b7ed86c0ec
Hyun-seok Jeon <what_is_computer@msn.com>
2024-02-10 08:43:27 +0900

    24.02.10 update
    24.02.10 update

--- HUSTOJ/admin/problem_export240130.php
+++ HUSTOJ/admin/problem_export240130.php
@@ -1,13 +1,3 @@
-<html>
-<head>
-  <meta http-equiv="Pragma" content="no-cache">
-  <meta http-equiv="Cache-Control" content="no-cache">
-  <meta http-equiv="Content-Language" content="zh-cn">
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <title>Problem Export</title>
-</head>
-<hr>
-
 <?php 
   require_once("../include/db_info.inc.php");
   require_once("admin-header.php");
