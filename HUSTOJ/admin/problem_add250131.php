09c3cf83949b05c79c75192fed0cfd38f865490e
Hyun-seok Jeon <what_is_computer@msn.com>
2025-02-08 08:22:16 +0900

    25.02.08 update
    25.02.08 update

---
+++ HUSTOJ/admin/problem_add250131.php
@@ -0,0 +1,159 @@
+<?php
+require_once ("admin-header.php");
+require_once("../include/check_post_key.php");
+if (!(isset($_SESSION[$OJ_NAME.'_'.'administrator']) || isset($_SESSION[$OJ_NAME.'_'.'contest_creator']) || isset($_SESSION[$OJ_NAME.'_'.'problem_editor']))) {
+  echo "<a href='../loginpage.php'>Please Login First!</a>";
+  exit(1);
+}
+
+require_once ("../include/db_info.inc.php");
+require_once ("../include/my_func.inc.php");
+require_once ("../include/problem.php");
+
+// contest_id
+$title = $_POST['title'];
+$title = str_replace(",", "&#44;", $title);
+$time_limit = $_POST['time_limit'];
+$memory_limit = $_POST['memory_limit'];
+
+$description = $_POST['description'];
+//$description = str_replace("<p>", "", $description); 
+//$description = str_replace("</p>", "<br />", $description);
+$description = str_replace(",", "&#44;", $description); 
+
+$input = $_POST['input'];
+//$input = str_replace("<p>", "", $input); 
+//$input = str_replace("</p>", "<br />", $input); 
+$input = str_replace(",", "&#44;", $input);
+
+$output = $_POST['output'];
+//$output = str_replace("<p>", "", $output); 
+//$output = str_replace("</p>", "<br />", $output);
+$output = str_replace(",", "&#44;", $output); 
+
+$sample_input = $_POST['sample_input'];
+$sample_output = $_POST['sample_output'];
+$test_input = $_POST['test_input'];
+$test_output = $_POST['test_output'];
+/* don't do this , we will left them empty for not generating invalid test data files 
+if ($sample_input=="") $sample_input="\n";
+if ($sample_output=="") $sample_output="\n";
+if ($test_input=="") $test_input="\n";
+if ($test_output=="") $test_output="\n";
+*/
+
+
+//+ by CSL
+if ($sample_input=="") $sample_input="\n";
+if ($sample_output=="") $sample_output="\n";
+if ($test_input=="") $test_input="\n";
+if ($test_output=="") $test_output="\n";
+
+
+$hint = $_POST['hint'];
+//$hint = str_replace("<p>", "", $hint); 
+//$hint = str_replace("</p>", "<br />", $hint); 
+$hint = str_replace(",", "&#44;", $hint);
+
+$source = $_POST['source'];
+
+$spj = $_POST['spj'];
+
+
+//+ by CSL
+$front = $_POST['front'];
+$rear = $_POST['rear'];
+$bann = $_POST['bann'];
+$credits = $_POST['credits'];
+
+
+if (false) {
+  $title = stripslashes($title);
+  $time_limit = stripslashes($time_limit);
+  $memory_limit = stripslashes($memory_limit);
+  $description = stripslashes($description);
+  $input = stripslashes($input);
+  $output = stripslashes($output);
+  $sample_input = stripslashes($sample_input);
+  $sample_output = stripslashes($sample_output);
+  $test_input = stripslashes($test_input);
+  $test_output = stripslashes($test_output);
+  $hint = stripslashes($hint);
+  $source = stripslashes($source);
+  $spj = stripslashes($spj);
+  $source = stripslashes($source);
+
+
+  //+ by CSL
+  $front = stripslashes($front);
+  $rear = stripslashes($rear);
+  $bann = stripslashes($bann);
+  $credits = stripslashes($credits);  
+
+
+}
+
+$title = ($title);
+$description = ($description);
+$input = ($input);
+$output = ($output);
+$hint = ($hint);
+//echo "->".$OJ_DATA."<-"; 
+//* by CSL
+$pid = addproblem($title, $time_limit, $memory_limit, $description, $input, $output, $sample_input, $sample_output, $hint, $source, $spj, $front, $rear, $bann, $credits, $OJ_DATA);
+
+
+$basedir = "$OJ_DATA/$pid";
+mkdir($basedir);
+if(strlen($sample_output) && !strlen($sample_input)) $sample_input = "0";
+if(strlen($sample_input)) mkdata($pid, "sample.in", $sample_input, $OJ_DATA);
+if(strlen($sample_output)) mkdata($pid, "sample.out", $sample_output, $OJ_DATA);
+if(strlen($test_output) && !strlen($test_input)) $test_input = "0";
+if(strlen($test_input)) mkdata($pid,"test.in", $test_input, $OJ_DATA);
+if(strlen($test_output)) mkdata($pid,"test.out", $test_output, $OJ_DATA);
+if(isset($_POST['remote_oj'])){
+	$remote_oj=$_POST['remote_oj'];
+	$remote_id=intval($_POST['remote_id']);
+	$sql="update problem set remote_oj=?,remote_id=? where problem_id=?";
+	pdo_query($sql,$remote_oj,$remote_id,$pid);
+	?>
+<form method=POST action=problem_add_page_<?php echo $remote_oj?>.php>
+<?php 
+	if($remote_oj=="luogu"){
+		$pre=mb_strpos($source,"P");
+		$pre=mb_substr($source,0,$pre+1);
+		$remote_id=intval(mb_substr($_POST['remote_id'],1));
+		echo "remote id :$remote_id";
+	
+	}else{
+		$pre=mb_strpos($source,"=");
+		$pre=mb_substr($source,0,$pre+1);
+	}
+?>
+<input name=url type=text size=100  class="input input-xxlarge" value="<?php echo $pre.(++$remote_id) ?>">
+  <input type=submit>
+</form>
+<script>// $("form").submit();</script>
+
+	<?php
+}
+
+$sql = "INSERT INTO `privilege` (`user_id`,`rightstr`) VALUES(?,?)";
+pdo_query($sql, $_SESSION[$OJ_NAME.'_'.'user_id'], "p$pid");
+$_SESSION[$OJ_NAME.'_'."p$pid"] = true;
+  
+echo "&nbsp;&nbsp;- <a href='javascript:phpfm($pid);'>Add more TestData now!</a>";
+/*  */
+?>
+
+<script src='../template/bs3/jquery.min.js' ></script>
+<script>
+function phpfm(pid){
+  //alert(pid);
+  $.post("phpfm.php",{'frame':3,'pid':pid,'pass':''},function(data,status){
+    if(status=="success"){
+      document.location.href="phpfm.php?frame=3&pid="+pid;
+    }
+  });
+}
+</script>
---
+++ HUSTOJ/admin/problem_export250131.php
@@ -0,0 +1,84 @@
+<?php 
+  require_once("../include/db_info.inc.php");
+  require_once("admin-header.php");
+
+  if (!(isset($_SESSION[$OJ_NAME.'_'.'administrator']) || isset($_SESSION[$OJ_NAME.'_'.'contest_creator']) || isset($_SESSION[$OJ_NAME.'_'.'problem_editor']))) {
+    echo "<a href='../loginpage.php'>Please Login First!</a>";
+    exit(1);
+  }
+
+  echo "<center><h3>".$MSG_PROBLEM."-".$MSG_EXPORT."</h3></center>";
+
+?>
+<html>
+<head>
+  <meta http-equiv="Pragma" content="no-cache">
+  <meta http-equiv="Cache-Control" content="no-cache">
+  <meta http-equiv="Content-Language" content="zh-cn">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>Problem Export</title>
+</head>
+<hr>
+
+<body leftmargin="30" >
+  <div class="container">
+    <br><br>
+    <!-- * by CSL-->
+    - CSL HUSTOJ 문제 XML 다운로드<br><br>
+    - CSL HUSTOJ의 문제 XML은 상위 버전의 CSL HUSTOJ에도 업로드가 됩니다.<br>
+    - 직접 만들거나 더 좋게 개선한 문제들을 다른 정보선생님들에게 나눔할 수 있습니다.<br><br>
+    - 연속번호 또는 개별번호 중 한가지 방법으로만 다운로드하세요.<br><br>
+    <br>
+    <form class="form-inline" action="problem_export_xml.php" method=post>
+      <div class="form-group">
+        <!-- * by CSL-->
+        <label>1) 연속번호 다운로드:</label>
+        <input class="form-control" name="start" type="text" placeholder="1001">
+      </div>
+      <div class="form-group">
+        <label> ~ </label>
+        <input class="form-control" name="end" type="text" placeholder="1009">
+      </div>
+      <br><br>
+      <div class="form-group">
+        <!-- * by CSL-->
+        <label>2) 개별번호 다운로드:</label>
+        <input class="form-control" name="in" type="text" placeholder="1001,1003,1005, ... ">
+      </div>
+      <br><br>
+        <!-- - by CSL-->
+        <!--
+     <div class="form-group">
+        <label>3) 为远程判题导出题面填写远程别名，正常导出请留空 for Remote judge only :</label>
+        <input class="form-control" name="remote_name" type="text" placeholder="my">
+      </div>
+      <br><br>
+         -->
+
+      <center>
+      <div class='form-group'>
+        <input type="hidden" name="do" value="do">
+        <!-- <input type="submit" name="submit" value="Export to XML Script"> -->
+
+        <!-- * by CSL -->
+        <button class='btn btn-default btn-sm' type=submit>다운로드</button>
+      </div>
+      </center>
+
+      <?php require_once("../include/set_post_key.php");?>
+    </form>
+
+    <br><br>
+    <!--
+    * from-to will working if empty IN <br>
+    * if using IN,from-to will not working.<br>
+    * IN can go with "," seperated problem_ids like [1000,1020]
+    -->
+
+    <!-- * by CSL-->
+    - 개별번호가 비어있어야만 연속번호로 다운로드가 됩니다.<br>
+    - 연속번호가 비어있어야만 개별번호로 다운로드가 됩니다.
+  </div>
+
+</body>
+</html>
